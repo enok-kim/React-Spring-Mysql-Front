8c5d1eea2c6b3ea3ff322e7bdce8394dc0af91e5
enok-kim <rladpshr2581@gmail.com>
2024-06-05 13:56:48 +0900

    헤더 컴포넌트 생성

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import './style.css';
+import { useNavigate } from 'react-router-dom';
+import { MAIN_PATH } from 'constant';
 
 //           component: 헤더 레이아웃          //
 export default function Header() {
 
+//           function: 네비게이트 함수        //
+const navigate = useNavigate();
+
+//           event handler: 로고클릭 이벤트 처리 함수        //
+const onLogoClickHandler = () => {
+  navigate(MAIN_PATH());
+}
+
 //           render: 헤더 레이아웃 랜더링         //
   return (
-    <div>index</div>
+    <div id='header'>
+      <div className="header-container">
+        <div className="header-left-box" onClick={onLogoClickHandler}>
+          <div className="icon-box">
+            <div className="icon logo-dark-icon"></div>
+          </div>
+          <div className="header-logo">{'Kanaki Board'}</div>
+        </div>
+        <div className="header-right-box"></div>
+      </div>
+    </div>
   )
 }
