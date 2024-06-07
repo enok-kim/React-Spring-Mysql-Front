db9baa9ecc85dd1f42b02124d35e11a41a97c6ba
enok-kim <rladpshr2581@gmail.com>
2024-06-07 16:42:53 +0900

    로그인 창 생성

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -12,7 +12,7 @@ interface Props {
   setValue: Dispatch<SetStateAction<string>>;
   error: boolean;
 
-  icon?: string;
+  icon?: 'eye-light-off-icon' | 'expand-right-light-icon' | 'eye-light-on-icon';
   onButtonClick?: () => void;
 
   message? : string,
@@ -20,34 +20,34 @@ interface Props {
   onKeyDown? : (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-            // component: Input Box 컴포넌트             //
+            // component : Input Box 컴포넌트             //
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
 
-//             state: properties             //
+//             state : properties             //
 const {label, type, value, placeholer,　error,icon,message} = props;
 const {setValue, onButtonClick, onKeyDown} = props;
 
-//             event handler: input 값 변경 이벤트 처리 함수          //
+//             event handler : input 값 변경 이벤트 처리 함수          //
 const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
   const {value} = event.target;
   setValue(value);
 };
 
-//             event handler: Keyboard 이벤트 처리 함수
+//             event handler : Keyboard 이벤트 처리 함수
 const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
   if(!onKeyDown) return;
   onKeyDown(event);
 };
 
-//             render: Input Box 랜더링             //
+//             render : Input Box 랜더링             //
   return (
     <div className='inputbox'>
       <div className='inputbox-label'>{label}</div>
       <div className= {error ? 'inputbox-container-error' : 'inputbox-container'}>
         <input ref={ref} type={type} className='input' placeholder={placeholer} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
         {onButtonClick !== undefined && (
-        <div className="icon-button">
+        <div className="icon-button" onClick={onButtonClick}>
         {icon !== undefined && <div className={`icon ${icon}`}></div>}
         </div>
         )}
--- src/views/Authentication/index.tsx
+++ src/views/Authentication/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, KeyboardEvent, useRef } from 'react'
 import './style.css'
 import { idText } from 'typescript'
+import InputBox from 'components/InputBox';
 
 //          component : 인증 화면 컴포넌트          //
 export default function Authentication() {
@@ -10,9 +11,82 @@ const [view, setView] = useState< 'sign-in' | 'sign-up' >('sign-in');
   
 //          component : sign in card 컴포넌트          //
 const SignInCard = () => {
+//          state : 이메일 요소 참조 상태                  //
+const emailRef = useRef<HTMLInputElement | null>(null);
+//          state : 이메일 상태                  //
+const passwordRef = useRef<HTMLInputElement | null>(null);
+//          state : 이메일 상태                  //
+const [email, setEmail] = useState<string>('');
+
+//          state : 패스워드 상태                  //
+const [password, setPassword] = useState<string>('');
+
+//          state : 패스워드 타입 상태                  //
+const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+
+//          state : 패스워드 버튼 아이콘 상태                  //
+const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+
+
+//          state : 에러 상태                  //
+const [error, setError] = useState<boolean>(false);
+
+//          event handler : 로그인 버튼 클릭 이벤트 처리          //
+const onSignInButtonClickHandler = () => {
+
+}
+//          event handler : 패스워드 버튼 클릭 이벤트 처리          //
+const onSignOnButtonClickHandler = () => {
+  setView('sign-up')
+}
+//          event handler : 패스워드 버튼 클릭 이벤트 처리          //
+const onPasswordButtonClickHandler = () => {
+  if(passwordType === 'text'){
+    setPasswordType('password');
+    setPasswordButtonIcon('eye-light-off-icon');
+  }else{
+    setPasswordType('text');
+    setPasswordButtonIcon('eye-light-on-icon');
+  }//else
+} // onPasswordButtonClickHandler
+//          event handler : 이메일 인풋 키 다운 이벤트 처리          //
+const onEmailKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+if (event.key !== 'Enter') return;
+if (!passwordRef.current) return;
+passwordRef.current.focus();
+}//onEmailKeyDownHandler
+//          event handler :         //
+const onPasswordKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) =>{
+  if (event.key !== 'Enter')return;
+}
+
+
 //          render : sign in 컴포넌트 렌더링       //  
 return (
-  <div id="auth-card"></div>
+  <div id="auth-card">
+    <div className="auth-card-box">
+      <div className="auth-card-top">
+        <div className="auth-card-title-box">
+          <div className="auth-card-title">{'Login'}</div>
+        </div>
+        <InputBox ref={emailRef} label='MailAdress' type='text' placeholer='メールアドレスを入力してください。'　error={error} value={email} setValue={setEmail} onKeyDown={onEmailKeyDownHandler} />
+        <InputBox ref={passwordRef} label='PassWord' type={passwordType} placeholer='パスワードを入力してください。'　error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
+      </div>
+      <div className="auth-card-bottom">
+        <div className="auth-signin-error-box">
+          <div className="auth-signin-error-message">
+            {'IDとパスワードが間違っています\n再入力お願いします。'}
+            </div>
+        </div>
+        <div className="black-large-full-button">{'Login'}</div>
+        <div className="auth-description-box">
+          <div className="auth-description">{'新規登録ですか？'}
+          <span className='auth-description-link' onClick={onSignOnButtonClickHandler}>{'SignUp'}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 };
 //          component : sign up card 컴포넌트          //
